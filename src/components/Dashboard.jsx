b8d409cc7880c7b61997c8ee501d676c6bfd0457
Christopher Iliyas <christopheriliyas@gmail.com>
2019-11-04 18:49:44 +0100

    finish dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,50 +4,51 @@ const Dashboard = () => {
     return (
         <div className="dashboard">
             <p>Dashboard</p>
+            <div className="filler-1"></div>
             <div className="dashboard__info">
                 <div className="dashboard__info--statistics">
                     <div className="dashboard__info--stat">
                         <div className="stat-icon stat-icon-purple">
-                            <i class="far fa-file-alt"></i>icon
+                            <i class="fas fa-sitemap"></i>
                         </div>
                         <div className="stat-text">
-                            <p>Lorem ipsum dolor sit amet</p>
-                            <h4>adipiscing elit</h4>
+                            <p>Departments</p>
+                            <h4>5 members</h4>
                         </div>
                     </div>
 
                     <div className="dashboard__info--stat">
                         <div className="stat-icon stat-icon-blue">
-                            <i class="far fa-file-alt"></i>icon
+                            <i class="fas fa-user"></i>
                         </div>
                         <div className="stat-text">
-                            <p>Lorem ipsum dolor sit amet</p>
-                            <h4>adipiscing elit</h4>
+                            <p>Members</p>
+                            <h4>24 members</h4>
                         </div>
                     </div>
 
                     <div className="dashboard__info--stat">
                         <div className="stat-icon stat-icon-turquoise">
-                            <i class="far fa-file-alt"></i>icon
+                            <i class="fas fa-users"></i>
                         </div>
                         <div className="stat-text">
-                            <p>Lorem ipsum dolor sit amet</p>
-                            <h4>adipiscing elit</h4>
+                            <p>Units</p>
+                            <h4>3 Units</h4>
                         </div>
                     </div>
 
                     <div className="dashboard__info--stat">
                         <div className="stat-icon stat-icon-green">
-                            <i class="far fa-file-alt"></i>icon
+                            <i class="fas fa-archive"></i>
                         </div>
                         <div className="stat-text">
-                            <p>Lorem ipsum dolor sit amet</p>
-                            <h4>adipiscing elit</h4>
+                            <p>Documents</p>
+                            <h4>12,097 files</h4>
                         </div>
                     </div>
                 </div>
-                <button class="new-document">
-                    <i className="fas fa-bell"></i> New Document
+                <button class="new-document-btn">
+                    <i class="fas fa-plus"></i> <p>New Document</p>
                 </button>
             </div>
         </div>
