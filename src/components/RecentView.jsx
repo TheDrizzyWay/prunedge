4dead03a7302d281edf6a1a2b5cd833cbb79b857
Christopher Iliyas <christopheriliyas@gmail.com>
2019-11-05 12:27:17 +0100

    finish activities component

--- src/components/RecentView.jsx
+++ src/components/RecentView.jsx
@@ -21,8 +21,8 @@ const RecentView = () => {
                         <i class="far fa-file-alt"></i>
                     </div>
                     <div className="doc-text">
-                        <h2>Lorem ipsum dolor sit amet</h2>
-                        <p>adipiscing elit</p>
+                        <h2>Presentation Slides</h2>
+                        <p>Today, 14:30 pm</p>
                     </div>
                 </div>
                 <div className="document">
@@ -30,8 +30,8 @@ const RecentView = () => {
                         <i class="far fa-file-alt"></i>
                     </div>
                     <div className="doc-text">
-                        <h2>Lorem ipsum dolor sit amet</h2>
-                        <p>adipiscing elit</p>
+                        <h2>Property Invoice</h2>
+                        <p>Today, 02:30 pm</p>
                     </div>
                 </div>
                 <div className="document">
@@ -39,8 +39,56 @@ const RecentView = () => {
                         <i class="far fa-file-alt"></i>
                     </div>
                     <div className="doc-text">
-                        <h2>Lorem ipsum dolor sit amet</h2>
-                        <p>adipiscing elit</p>
+                        <h2>Alex Interview Report</h2>
+                        <p>Today, 14:30 pm</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card2">
+                <div className="header">
+                    <h1>Recent Activities</h1>
+                    <p>View All</p>
+                </div>
+
+                <div className="activity">
+                    <div className="doc-header">
+                        <div className="icon-container">
+                            <i className="fas fa-check-circle"></i>
+                        </div>
+                        <h4>Document Approval</h4>
+                        <p>4 mins ago</p>
+                    </div>
+                    <div className="doc-content">
+                        <div className="user">
+                            <div className="user-icon">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="user-comment">
+                                <p>Segun Oni has approved, <span>Business proposal document.docx</span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <hr/>
+                </div>
+
+                <div className="activity">
+                    <div className="doc-header">
+                        <div className="icon-container">
+                            <i className="fas fa-exclamation-triangle danger"></i>
+                        </div>
+                        <h4>Document Rejection</h4>
+                        <p>4 mins ago</p>
+                    </div>
+                    <div className="doc-content">
+                        <div className="user">
+                            <div className="user-icon">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="user-comment">
+                                <p>Lorem ipsum dolor sit amet, <span>consectetur adipiscing elit</span></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
